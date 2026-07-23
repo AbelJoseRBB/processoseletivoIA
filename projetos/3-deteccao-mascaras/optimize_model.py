5fe68ac444ccb40df6d13b5b05d348e4dd6796b4
AbelJoseRBB <abeljose018@gmail.com>
2026-07-23 23:42:48 +0000

    Feat: Treinamento e otimização implementados

--- projetos/3-deteccao-mascaras/optimize_model.py
+++ projetos/3-deteccao-mascaras/optimize_model.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
-
+import shutil
+import os
 # ---------------------------------------------------------------------------
 # Projeto 3 — Otimização do Modelo (Exportação para Edge)
 #
@@ -8,8 +9,33 @@ from ultralytics import YOLO
 #   2. Exportar para TensorFlow Lite via model.export(format="tflite")
 #      (a Ultralytics gera automaticamente "model.tflite" na mesma pasta)
 # ---------------------------------------------------------------------------
+if not os.path.exists("model.pt"):
+    print(f"Erro, arquivo model.pt nao encontrado")
+    exit()
 
-# insira seu código aqui
+model = YOLO("model.pt")
+
+try:
+    model.export(format= "tflite")
+    tflite_16 = "model_saved_model/model_float16.tflite"
+    tflite_32 = "model_saved_model/model_float32.tflite"
+    model_path = "model.tflite"
+
+    if os.path.exists(tflite_16):
+        os.rename(tflite_path, model_path)
+        shutil.rmtree("model_saved_model")
+    elif os.path.exists(tflite_32):
+        os.rename(tflite_32, model_path)
+        shutil.rmtree("model_saved_model")
+    elif not os.path.exists(model_path):
+        print("Atenção: A exportação terminou, mas o arquivo 'model.tflite' não foi encontrado na raiz.")
+
+    if os.path.exists(model_path):
+        print("O modelo otimizado foi salvo como 'model.tflite' na raiz do projeto.")
+
+except Exception as e:
+    print(f"Ocorreu um erro durante a exportação: {e}")
+
 
 # Dica de estrutura (não é obrigatório seguir exatamente assim):
 #
--- projetos/3-deteccao-mascaras/train_model.py
+++ projetos/3-deteccao-mascaras/train_model.py
@@ -1,6 +1,29 @@
 import shutil
-
+import os
 from ultralytics import YOLO
+
+model = YOLO("yolo11n.pt")
+
+model.train(
+    data = "dataset/data.yaml",
+    epochs = 20,
+    batch = 8,
+    device = "cpu",
+    project = "runs/detect",
+    name = "train",
+    exist_ok = True
+)
+
+best_weights = os.path.join("runs", "detect", "train", "weights", "best.pt")
+final_model = "model.pt"
+
+if os.path.exists(best_weights):
+    shutil.copy(best_weights, final_model)
+    print(f"Modelo salvo com sucesso como '{final_model}'")
+else:
+    print(f"Treinamento falhou. Não foi possível encontrar '{best_weights}'")
+
+
 
 # ---------------------------------------------------------------------------
 # Projeto 3 — Detecção de Máscaras Faciais (Fine-tuning do YOLO11n)
